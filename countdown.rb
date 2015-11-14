c3ea441fa603ea8f47d19393c374215891728aeb
iSadia <sadia.bruce@gmail.com>
2015-11-14 11:23:41 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,30 @@
 #write your code here
 
-def countdown
+def countdown(newyear)
+  
+  x = 11
+
+  while x > 1 do
+    x = x -= 1
+    puts "#{x} SECOND(S)!"
+  end
+
+phrase = "HAPPY NEW YEAR!"
+    return phrase
+
+end
+
+def countdown_with_sleep(newyear)
+  
+  x = 11
+
+  while x > 1 do
+    sleep 1
+    x = x -= 1
+    puts "#{x} SECOND(S)!"
+  end
+
+phrase = "HAPPY NEW YEAR!"
+    return phrase
+
 end
